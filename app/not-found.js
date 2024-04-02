bb9948af0bc3a9535ad8a7cf009893917ccdc5b0
Olaoti <omotolaolaoti@gmail.com>
2024-04-02 15:52:48 +0100

    404 page

---
+++ app/not-found.js
@@ -0,0 +1,15 @@
+"use client";
+import React from 'react'
+
+function notFound() {
+  return (
+    <div className='not-found'>
+        <h1>Oops!</h1>
+        <h5>This page does not exist</h5>
+      <p>Trust me already? Even when we are both lost in reading, I would find your path back home!</p>
+      <button>GO HOME</button>
+    </div>
+  )
+}
+
+export default notFound
