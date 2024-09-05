e90a87cd9117e05943e70c8948a81ba9210773cb
Olaoti <omotolaolaoti@gmail.com>
2024-09-05 22:39:38 +0100

    wrapping fixed

--- app/Components/HeadComments.js
+++ app/Components/HeadComments.js
@@ -6,8 +6,13 @@ function HeadComments({username, payload}) {
       <div className='initial'>
         {username.slice(0,1)}
       </div>
-    <div><h3>{username}</h3>
-    <p>{payload}</p></div>
+    <div>
+      <h3>{username}</h3>
+        <pre style={{ whiteSpace: 'pre-wrap', wordBreak: 'break-word' }}>
+        <p>{payload}</p>
+
+        </pre>
+        </div>
     </div>
   )
 }
