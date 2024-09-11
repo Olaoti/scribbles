97ab5f736cf6a59f245416ebb10858730b9d216a
Olaoti <omotolaolaoti@gmail.com>
2024-09-11 13:02:49 +0100

    formspree addedd

--- app/Components/Modal.js
+++ app/Components/Modal.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Modal({msg, type}) {
   return (
     <div className={`modal ${type}`}>
-      <h3>{type=='success'?('Success'):('Error')}!</h3>
+      <h3>{type}!</h3>
       <p>{msg}</p>
     </div>
   )
--- app/about-me/page.js
+++ app/about-me/page.js
@@ -1,31 +1,32 @@
 'use client'
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import Modal from '../Components/Modal'
-import { useForm, ValidationError } from '@formspree/react';
+import { useForm, ValidationError, reset} from '@formspree/react';
 
 function page() {
   const [name, setName] = useState('')
   const [mail, setMail] = useState('')
   const [message, setMessage] = useState('')
   const [subject, setSubject] = useState('')
-  const [submitted, setSubmitted] = useState(false)
   const [state, handleSubmit] = useForm("movazqbl");
-  if (state.succeeded) {
+  const [submitted, setSubmitted] = useState(false)
+useEffect(()=>{
+  if(state.succeeded){
     setSubmitted(true)
-    showModal()
-}
-function showModal(){
-  setTimeout(()=>{
-    setSubmitted(false)
-  },4000)
-}
-if(submitted){
-  return <Modal type='info' msg="Mail sent! You'd hear from me soon"/>
-}
+    setMail('')
+    setName('')
+    setSubject('')
+    setMessage('')
+    setTimeout(()=>{
+      setSubmitted(false)
+    },4000)
+    }
+},[state.succeeded])
 
-  
+
   return (
     <div className='about-me container'>
+     {submitted&&<Modal type='info' msg="Mail sent! You'd hear from me soon"/>}
       <div className='wrap'>
         <h2>HELLO WORLD !</h2>
         <p>Welcome home! I'm Omotola, and I love writing about all the beautiful things in life. Some pieces may be short stories, Tech, others poems, and a few might escape categorization. So, grab a cup of coffee, and let's take a sip through my world :)</p>
@@ -33,9 +34,9 @@ if(submitted){
         <form onSubmit={handleSubmit}>
         <h2>Say Hi!</h2>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' name='name' id='name' onChange={(e) => setName(e.target.value)} value={name}/>
+        <input type='text' name='name' id='name' required onChange={(e) => setName(e.target.value)} value={name}/>
         <label htmlFor='email'>Your Email</label>
-        <input type='email' name="_replyto" id='mail' onChange={(e) => setMail(e.target.value)} value={mail}/>
+        <input type='email' required name="_replyto" id='mail' onChange={(e) => setMail(e.target.value)} value={mail}/>
         <ValidationError 
         prefix="Email" 
         field="email"
@@ -44,14 +45,14 @@ if(submitted){
         <label htmlFor='subject'>Subject</label>
         <input type='subject' name='subject' id='subject' onChange={(e) => setSubject(e.target.value)} value={subject}/>
         <label htmlFor='message'>Your Message</label>
-        <textarea id='message' name='message' rows={10} onChange={(e) => setMessage(e.target.value)} value={message}/>
+        <textarea id='message' required name='message' rows={10} onChange={(e) => setMessage(e.target.value)} value={message}/>
         <ValidationError 
         prefix="Message" 
         field="message"
         errors={state.errors}
       />
            <button type="submit" disabled={state.submitting}>
-        Submit
+        {state.submitting?('Submitting...'):('Submit')}
       </button>
       </form>
       </div>
